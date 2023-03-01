1f7500dfce919a015aec442ed38b3ad7cb715e01
HyeonJi Na <dev.hyeonji@gmail.com>
2023-03-01 10:41:41 +0900

    Day32

--- Problem083.py
+++ Problem083.py
@@ -1,12 +1,8 @@
-r, g, b = input().split()
-r = int(r)
-g = int(g)
-b = int(b)
-
-n=0
-for i in range(0, r) :
-  for j in range(0, g) :
-      for k in range(0, b):
-        n=n+1
-        print(i, j, k)
-print(n)
+r,g,b = map(int,input().split())
+count=0;
+for i in range(r):
+    for j in range(g):
+        for k in range(b):
+            print('%d %d %d' %(i,j,k))
+            count+=1
+print(count)
